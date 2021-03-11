94c4378711324514d99b9f73962bf1cc858897fb
grvsharma1810 <gs8280601@gmail.com>
2021-03-11 11:37:34 +0530

    form -continued

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-// document.querySelectorAll('input[data-error]').forEach(inpEl => {
-//     inpEl.addEventListener('input', () => {
-//         console.log("Hi");
-//         inpEl.parentElement.removeAttribute('data-error')
-//     })
-// })
+document.querySelectorAll('.do-validation').forEach(forms => {
+    forms.addEventListener('submit',function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let formFields = event.target;
+        console.log(formFields);
+        // formFields.forEach()
+    })
+    // inpEl.addEventListener('input', () => {
+    //     console.log("Hi");
+    //     inpEl.parentElement.removeAttribute('data-error')
+    // })
+})
+// (function () {
+//     'use strict'
+
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll('.needs-validation')
+
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//         .forEach(function (form) {
+//             form.addEventListener('submit', function (event) {
+//                 if (!form.checkValidity()) {
+//                 event.preventDefault()
+//                 event.stopPropagation()
+//                 }
+
+//                 form.classList.add('was-validated')
+//             }, false)
+//         })
+// })()
